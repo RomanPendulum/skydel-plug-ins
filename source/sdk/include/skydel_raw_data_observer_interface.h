7eaaa86a1dd8e85e7b8fba8f3a712539e5b38b49
Roman Marchenkov <roman.marchenkov@pendulum-instruments.com>
2020-12-27 23:10:15 +0300

    SkydelRuntimeRawDataObserver::TimedRawData populated with meaningful members.

--- source/sdk/include/skydel_raw_data_observer_interface.h
+++ source/sdk/include/skydel_raw_data_observer_interface.h
@@ -3,15 +3,64 @@
 
 #include <QObject>
 
+#include <chrono>
+#include <array>
+
 class SkydelRuntimeRawDataObserver : public QObject
 {
 public:
   virtual ~SkydelRuntimeRawDataObserver() = default;
 
+  struct SignalRawData
+  {
+      SignalRawData():
+          signalType("Unknown"),
+          pseudoRange(0.0),
+          phaseRange(0.0),
+          pseudoRangeRate(0.0),
+          phaseRangeRate(0.0),
+          ageSeconds(0.0)
+      {}
+
+      std::string signalType;
+
+      double pseudoRange;
+      double phaseRange;
+      double pseudoRangeRate;
+      double phaseRangeRate;
+
+      unsigned long ageSeconds;
+  };
+
+  struct SatelliteRawData
+  {
+      SatelliteRawData():
+          sys("Unknown"),
+          id (0),
+          subId(0)
+      {}
+
+      std::string sys;
+      int id;
+      int subId; // GLONASS: frequency slot [-7, +6]
+
+      std::vector<SignalRawData> signalsData;
+  };
+
+
   struct TimedRawData
   {
-    int64_t time;            // ms
-    // More members to follow here
+      using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
+
+      TimedRawData():
+          gpsToUtcLs(0)
+      {}
+
+      TimePoint gpsTime;
+      int gpsToUtcLs; // GPS-UTC leap seconds
+
+      std::vector<SatelliteRawData> satellitesData;
+      std::array<double, 3> ecefPosition;
   };
 
   virtual void pushRawData(const TimedRawData &) = 0;
